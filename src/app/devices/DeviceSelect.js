1c74ec04cfca5110f8acbe18518e56a305187efb
pulsation <philippe.sam-long@laposte.net>
2013-08-29 17:34:46 +0200

    Use object store instead of options for selector

--- src/app/devices/DeviceSelect.js
+++ src/app/devices/DeviceSelect.js
@@ -1,12 +1,11 @@
-define(['dojo/_base/declare', 'dijit/registry',  'dijit/form/Select'], 
-  function(declare, registry, Select) {
+define(['dojo/_base/declare', 'dijit/registry',  'dijit/form/Select', 'dojo/store/Memory', "dojo/data/ObjectStore", "./Devices", "dojo/store/Observable"], 
+  function(declare, registry, Select, Memory, ObjectStore, Devices, Observable) {
     return declare(Select, {
       postCreate: function () {
         this.inherited(arguments);
-        this.options = [
-         { label: "Über phone", value: "uber" },
-         { label: "Terrible phone", value: "terrible"}
-        ];
+        var store = Observable(new Memory());
+        this.setStore( new ObjectStore({objectStore: store}));
+        Devices.populateStore(store);
       }
     });
   });
