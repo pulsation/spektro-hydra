a98d322bec18a36d578690f079e449de0b4da06b
pulsation <philippe.sam-long@laposte.net>
2013-08-30 15:59:59 +0200

    Remove useless imports

--- src/app/devices/DeviceSelect.js
+++ src/app/devices/DeviceSelect.js
@@ -1,5 +1,5 @@
-define(['dojo/_base/declare', 'dijit/registry',  'dijit/form/Select', 'dojo/store/Memory', "dojo/data/ObjectStore", "dojo/store/Observable", "./DevicesStore"],
-  function(declare, registry, Select, Memory, ObjectStore, Observable, DevicesStore) {
+define(['dojo/_base/declare', 'dijit/form/Select', "dojo/data/ObjectStore", "dojo/store/Observable", "./DevicesStore"],
+  function(declare, Select, ObjectStore, Observable, DevicesStore) {
     return declare(Select, {
       labelAttr: "model",
       postCreate: function () {
