5e57a439264d8304ddb5542cf4da8e3a1b8c89be
pulsation <philippe.sam-long@laposte.net>
2013-09-04 12:22:37 +0200

    Add submodule for dojo-pouchdb-store

--- src/app/devices/DevicesStore.js
+++ src/app/devices/DevicesStore.js
@@ -1,4 +1,4 @@
-define (["dojo/_base/declare", "../data/PouchStore"],
+define (["dojo/_base/declare", "dojo-pouchdb-store/PouchStore"],
         function(declare, SpektroPouchStore) {
           return declare(SpektroPouchStore, {
             pouchQuery:  { view: "selector/devices", options: { group: true, reduce: true } },
